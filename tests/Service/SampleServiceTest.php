17c7ffed2c272b06ce6d287b6253a50bd3c818bf
arm-band <bre4tn.let@gmail.com>
2020-11-20 23:56:05 +0900

    - composer パッケージのアップデート - テストコード若干修正

--- tests/Service/SampleServiceTest.php
+++ tests/Service/SampleServiceTest.php
@@ -2,12 +2,18 @@
 
 namespace DietcubeKyokotsu\Service;
 
-class SampleServiceTest extends \PHPUnit_Framework_TestCase
+class SampleServiceTest extends \PHPUnit\Framework\TestCase
 {
     public function testSayHello()
     {
         $sample_service = new SampleService();
 
         $this->assertEquals('Hello, welcome to Dietcube.', $sample_service->sayHello());
+    }
+    public function testSayKyokotsu()
+    {
+        $sample_service = new SampleService();
+
+        $this->assertEquals('HANAHADASHI', $sample_service->sayKyokotsu());
     }
 }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,15 @@
 <?php
 
 require dirname(__DIR__) . '/vendor/autoload.php';
+
+use \Dotenv\Dotenv;
+
+const ENV_PATH = '/../';
+const ENV = '.env';
+
+// find environment file
+$dot_env = __DIR__ . ENV_PATH . ENV;
+if (is_readable($dot_env)) {
+    $dotenv = Dotenv::createImmutable(__DIR__ . ENV_PATH);
+    $dotenv->load();
+}
